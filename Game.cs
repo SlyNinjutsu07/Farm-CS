0534edbb9a29135e884a0c335bcb4713233bc745
Sly <slyninjutsu07@gmail.com>
2024-07-15 14:23:40 -0400

    wip

--- Game.cs
+++ Game.cs
@@ -7,7 +7,7 @@ class Game{
     public static void Main(string[] args){
         Title = "Farm Game";
 
-        MainMenu mm = new MainMenu();
+        /* MainMenu mm = new MainMenu();
         mainmenu:
         mm.PrintOptions();
         mm.ReadInput();
@@ -16,15 +16,27 @@ class Game{
         else goto farm;
 
         farm:
-        throw new NotImplementedException();
-        // Shop shop = new Shop();
+        throw new NotImplementedException();*/
+        
+        string[][] map;
+        string[] filetext = File.ReadAllLines("file.txt");
 
-        // // game:
-        // // shop.PrintOptions();
-        // // shop.ReadInput();
-        // // Clear();
-        // // goto game;
+        map = new string[filetext.Length][];
 
-        //ReadKey();
+        for(int i = 0; i < filetext.Length; i++){
+            map[i] = new string[filetext[i].Length];
+            for(int j = 0; j < filetext[i].Length; j++){
+                map[i][j] = filetext[i].Substring(j, 1);
+            }
+        }
+
+        for(int i = 0; i < map.Length; i++){
+            for(int j = 0; j < map[i].Length; j++){
+                Write(map[i][j]);
+            }
+            WriteLine();
+        }
+
+        ReadKey();
     }
 }
